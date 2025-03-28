bbe59ab684fc1b2fb8fff34ff830401e8450ec04
Harrison Okins <harrison@scientist.com>
2025-03-28 14:28:48 -0500

    Set up Bad/Good examples

---
+++ bad-tests/timezone.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class BadTests
+  class Timezone < Test::Unit::TestCase
+    def test_start_of_year
+      start_of_year = Time.new(2025, 1, 1)
+      assert_equal start_of_year.year, 2025
+      assert_equal start_of_year.month, 1
+      assert_equal start_of_year.day, 1
+      assert_equal start_of_year.zone, 'UTC'
+    end
+  end
+end
---
+++ good-tests/timezone.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+
+class GoodTests
+  class TimezoneTest < Test::Unit::TestCase
+    def setup
+      # Save the original timezone for later
+      @original_timezone = ENV['TZ']
+
+      # Set the timezone to UTC for these tests
+      ENV['TZ'] = 'UTC'
+    end
+
+    def teardown
+      # Reset the timezone
+      ENV['TZ'] = @original_timezone
+    end
+
+    def test_start_of_year
+      start_of_year = Time.new(2025, 1, 1)
+      assert_equal start_of_year.year, 2025
+      assert_equal start_of_year.month, 1
+      assert_equal start_of_year.day, 1
+      assert_equal start_of_year.zone, 'UTC'
+    end
+  end
+end
