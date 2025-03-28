af938db6018939b8cff87f92a135d8ec8e61ec8d
Harrison Okins <harrison@scientist.com>
2025-03-28 15:02:29 -0500

    Add a database

--- test_helper.rb
+++ test_helper.rb
@@ -1,1 +1,23 @@
+require 'sqlite3'
 require 'test/unit'
+
+Test::Unit.at_start do
+  # Set up the database
+  @db = SQLite3::Database.new "test.db"
+
+  # Create a table
+  @db.execute <<-SQL
+    create table test_data (
+      name varchar(30),
+      val int
+    );
+  SQL
+end
+
+Test::Unit.at_exit do
+  # Cleanup the test data
+  @db.execute "drop table test_data"
+
+  # Close the database
+  @db.close
+end
