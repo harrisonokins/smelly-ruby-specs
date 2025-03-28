3aab631e9555769fe2c91d9e5baa50c15173027d
Harrison Okins <harrison@scientist.com>
2025-03-28 15:54:17 -0500

    Expose the SQLite connection via a `DB` module

--- test_helper.rb
+++ test_helper.rb
@@ -1,23 +1,35 @@
 require 'sqlite3'
 require 'test/unit'
 
+module DB
+  @@db = nil
+  
+  def self.connection
+    @@db
+  end
+  
+  def self.connection=(db)
+    @@db = db
+  end
+end
+
 Test::Unit.at_start do
   # Set up the database
-  @db = SQLite3::Database.new "test.db"
+  DB.connection = SQLite3::Database.new 'test.db'
 
   # Create a table
-  @db.execute <<-SQL
-    create table test_data (
-      name varchar(30),
-      val int
-    );
+  DB.connection.execute <<-SQL
+    CREATE TABLE users(
+      id INTEGER NOT NULL PRIMARY KEY,
+      name TEXT
+    )
   SQL
 end
 
 Test::Unit.at_exit do
   # Cleanup the test data
-  @db.execute "drop table test_data"
+  DB.connection.execute 'DROP TABLE users'
 
   # Close the database
-  @db.close
+  DB.connection.close
 end
