ed23e4235142f69297a136254713f023687da87f
Harrison Okins <harrisonokins@gmail.com>
2025-04-04 15:15:08 -0500

    Track SQL queries in `debug.log`

--- test_helper.rb
+++ test_helper.rb
@@ -1,7 +1,11 @@
+require 'logger'
 require 'pry'
 require 'sqlite3'
 require 'test/unit'
 require 'mocha/test_unit'
+
+# Create a logfile
+@log = Logger.new(File.open('debug.log', 'w'))
 
 module DB
   @@db = nil
@@ -18,6 +22,9 @@ end
 Test::Unit.at_start do
   # Set up the database
   DB.connection = SQLite3::Database.new 'test.db'
+
+  # Log all SQL commands
+  DB.connection.trace { |sql| @log.debug(sql) }
 end
 
 Test::Unit.at_exit do
